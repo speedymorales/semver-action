ae11abb83b8f3fa1d78e958f18e409d64fd2ffba
Jose Morales <speedymorales5@gmail.com>
2024-08-03 17:44:41 -0400

    Adds additional logging

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const _ = require('lodash')
 const cc = require('@conventional-commits/parser')
 const semver = require('semver')
 
-async function main () {
+async function main() {
   const token = core.getInput('token')
   const branch = core.getInput('branch')
   const gh = github.getOctokit(token)
@@ -14,19 +14,37 @@ async function main () {
   const noVersionBumpBehavior = core.getInput('noVersionBumpBehavior')
   const noNewCommitBehavior = core.getInput('noNewCommitBehavior')
   const prefix = core.getInput('prefix') || ''
-  const additionalCommits = core.getInput('additionalCommits').split('\n').map(l => l.trim()).filter(l => l !== '')
+  const additionalCommits = core
+    .getInput('additionalCommits')
+    .split('\n')
+    .map((l) => l.trim())
+    .filter((l) => l !== '')
   const fromTag = core.getInput('fromTag')
   const maxTagsToFetch = _.toSafeInteger(core.getInput('maxTagsToFetch') || 10)
-  const fetchLimit = (maxTagsToFetch < 1 || maxTagsToFetch > 100) ? 10 : maxTagsToFetch
+  const fetchLimit =
+    maxTagsToFetch < 1 || maxTagsToFetch > 100 ? 10 : maxTagsToFetch
 
   const bumpTypes = {
-    major: core.getInput('majorList').split(',').map(p => p.trim()).filter(p => p),
-    minor: core.getInput('minorList').split(',').map(p => p.trim()).filter(p => p),
-    patch: core.getInput('patchList').split(',').map(p => p.trim()).filter(p => p),
-    patchAll: (core.getInput('patchAll') === true || core.getInput('patchAll') === 'true')
+    major: core
+      .getInput('majorList')
+      .split(',')
+      .map((p) => p.trim())
+      .filter((p) => p),
+    minor: core
+      .getInput('minorList')
+      .split(',')
+      .map((p) => p.trim())
+      .filter((p) => p),
+    patch: core
+      .getInput('patchList')
+      .split(',')
+      .map((p) => p.trim())
+      .filter((p) => p),
+    patchAll:
+      core.getInput('patchAll') === true || core.getInput('patchAll') === 'true'
   }
 
-  function outputVersion (version) {
+  function outputVersion(version) {
     core.exportVariable('next', `${prefix}v${version}`)
     core.exportVariable('nextStrict', `${prefix}${version}`)
 
@@ -41,7 +59,8 @@ async function main () {
   if (!fromTag) {
     // GET LATEST + PREVIOUS TAGS
 
-    const tagsRaw = await gh.graphql(`
+    const tagsRaw = await gh.graphql(
+      `
       query lastTags (
         $owner: String!
         $repo: String!
@@ -66,15 +85,18 @@ async function main () {
         }
       }
     `,
-    {
-      owner,
-      repo,
-      fetchLimit
-    })
+      {
+        owner,
+        repo,
+        fetchLimit
+      }
+    )
 
     const tagsList = _.get(tagsRaw, 'repository.refs.nodes', [])
     if (tagsList.length < 1) {
-      return core.setFailed('Couldn\'t find the latest tag. Make sure you have at least one tag created first!')
+      return core.setFailed(
+        "Couldn't find the latest tag. Make sure you have at least one tag created first!"
+      )
     }
 
     let idx = 0
@@ -97,9 +119,15 @@ async function main () {
 
     if (!latestTag) {
       if (prefix) {
-        return core.setFailed(`None of the ${fetchLimit} latest tags are valid semver or match the specified prefix!`)
+        return core.setFailed(
+          `None of the ${fetchLimit} latest tags are valid semver or match the specified prefix!`
+        )
       } else {
-        return core.setFailed(skipInvalidTags ? `None of the ${fetchLimit} latest tags are valid semver!` : 'Latest tag is invalid (does not conform to semver)!')
+        return core.setFailed(
+          skipInvalidTags
+            ? `None of the ${fetchLimit} latest tags are valid semver!`
+            : 'Latest tag is invalid (does not conform to semver)!'
+        )
       }
     }
 
@@ -107,7 +135,8 @@ async function main () {
   } else {
     // GET SPECIFIC TAG
 
-    const tagRaw = await gh.graphql(`
+    const tagRaw = await gh.graphql(
+      `
       query singleTag ($owner: String!, $repo: String!, $tag: String!) {
         repository (owner: $owner, name: $repo) {
           ref(qualifiedName: $tag) {
@@ -118,11 +147,13 @@ async function main () {
           }
         }
       }
-    `, {
-      owner,
-      repo,
-      tag: `refs/tags/${prefix}${fromTag}`
-    })
+    `,
+      {
+        owner,
+        repo,
+        tag: `refs/tags/${prefix}${fromTag}`
+      }
+    )
 
     latestTag = _.get(tagRaw, 'repository.ref')
 
@@ -133,7 +164,9 @@ async function main () {
       latestTag.name = latestTag.name.replace(prefix, '')
     }
     if (!semver.valid(latestTag.name)) {
-      return core.setFailed('Provided tag is invalid! (does not conform to semver)')
+      return core.setFailed(
+        'Provided tag is invalid! (does not conform to semver)'
+      )
     }
 
     core.info(`Comparing against provided tag: ${prefix}${latestTag.name}`)
@@ -175,18 +208,26 @@ async function main () {
   if (!commits || commits.length < 1) {
     switch (noNewCommitBehavior) {
       case 'current': {
-        core.info(`Couldn't find any commits between branch HEAD and latest tag. Exiting with current as next version...`)
+        core.info(
+          "Couldn't find any commits between branch HEAD and latest tag. Exiting with current as next version..."
+        )
         outputVersion(semver.clean(latestTag.name))
         return
       }
       case 'silent': {
-        return core.info(`Couldn't find any commits between branch HEAD and latest tag. Exiting silently...`)
+        return core.info(
+          "Couldn't find any commits between branch HEAD and latest tag. Exiting silently..."
+        )
       }
       case 'warn': {
-        return core.warning(`Couldn't find any commits between branch HEAD and latest tag.`)
+        return core.warning(
+          "Couldn't find any commits between branch HEAD and latest tag."
+        )
       }
       default: {
-        return core.setFailed(`Couldn't find any commits between branch HEAD and latest tag.`)
+        return core.setFailed(
+          "Couldn't find any commits between branch HEAD and latest tag."
+        )
       }
     }
   }
@@ -197,28 +238,43 @@ async function main () {
   const minorChanges = []
   const patchChanges = []
   for (const commit of commits) {
+    core.info(`Processing commit message: ${commit.commit.message}`)
     try {
-      const cAst = cc.toConventionalChangelogFormat(cc.parser(commit.commit.message))
+      const cAst = cc.toConventionalChangelogFormat(
+        cc.parser(commit.commit.message)
+      )
       if (bumpTypes.major.includes(cAst.type)) {
         majorChanges.push(commit.commit.message)
-        core.info(`[MAJOR] Commit ${commit.sha} of type ${cAst.type} will cause a major version bump.`)
+        core.info(
+          `[MAJOR] Commit ${commit.sha} of type ${cAst.type} will cause a major version bump.`
+        )
       } else if (bumpTypes.minor.includes(cAst.type)) {
         minorChanges.push(commit.commit.message)
-        core.info(`[MINOR] Commit ${commit.sha} of type ${cAst.type} will cause a minor version bump.`)
+        core.info(
+          `[MINOR] Commit ${commit.sha} of type ${cAst.type} will cause a minor version bump.`
+        )
       } else if (bumpTypes.patchAll || bumpTypes.patch.includes(cAst.type)) {
         patchChanges.push(commit.commit.message)
-        core.info(`[PATCH] Commit ${commit.sha} of type ${cAst.type} will cause a patch version bump.`)
+        core.info(
+          `[PATCH] Commit ${commit.sha} of type ${cAst.type} will cause a patch version bump.`
+        )
       } else {
-        core.info(`[SKIP] Commit ${commit.sha} of type ${cAst.type} will not cause any version bump.`)
+        core.info(
+          `[SKIP] Commit ${commit.sha} of type ${cAst.type} will not cause any version bump.`
+        )
       }
       for (const note of cAst.notes) {
         if (note.title === 'BREAKING CHANGE') {
           majorChanges.push(commit.commit.message)
-          core.info(`[MAJOR] Commit ${commit.sha} has a BREAKING CHANGE mention, causing a major version bump.`)
+          core.info(
+            `[MAJOR] Commit ${commit.sha} has a BREAKING CHANGE mention, causing a major version bump.`
+          )
         }
       }
     } catch (err) {
-      core.info(`[INVALID] Skipping commit ${commit.sha} as it doesn't follow conventional commit format.`)
+      core.info(
+        `[INVALID] Skipping commit ${commit.sha} as it doesn't follow conventional commit format.`
+      )
     }
   }
 
@@ -232,27 +288,41 @@ async function main () {
   } else {
     switch (noVersionBumpBehavior) {
       case 'current': {
-        core.info('No commit resulted in a version bump since last release! Exiting with current as next version...')
+        core.info(
+          'No commit resulted in a version bump since last release! Exiting with current as next version...'
+        )
         outputVersion(semver.clean(latestTag.name))
         return
       }
       case 'patch': {
-        core.info('No commit resulted in a version bump since last release! Defaulting to using PATCH...')
+        core.info(
+          'No commit resulted in a version bump since last release! Defaulting to using PATCH...'
+        )
         bump = 'patch'
         break
       }
       case 'silent': {
-        return core.info('No commit resulted in a version bump since last release! Exiting silently...')
+        return core.info(
+          'No commit resulted in a version bump since last release! Exiting silently...'
+        )
       }
       case 'warn': {
-        return core.warning('No commit resulted in a version bump since last release!')
+        return core.warning(
+          'No commit resulted in a version bump since last release!'
+        )
       }
       default: {
-        return core.setFailed('No commit resulted in a version bump since last release!')
+        return core.setFailed(
+          'No commit resulted in a version bump since last release!'
+        )
       }
     }
   }
-  core.info(`\n>>> Will bump version ${prefix}${latestTag.name} using ${bump.toUpperCase()}\n`)
+  core.info(
+    `\n>>> Will bump version ${prefix}${
+      latestTag.name
+    } using ${bump.toUpperCase()}\n`
+  )
   core.setOutput('bump', bump || 'none')
 
   // BUMP VERSION
